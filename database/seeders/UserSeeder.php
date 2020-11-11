870a420fb0eb54e19592165f0794bbfa9db14d88
ahmedgalaldev <ahmedgalaldev@gmail.com>
2020-11-11 13:21:23 +0200

    add admin seeds

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -18,7 +19,7 @@ class UserSeeder extends Seeder
         DB::table('users')->insert([
             'name' => 'admin',
             'email' =>  'admin@admin.com',
-            'password' => bcrypt('admin12345'),
+            'password' => Hash::make('admin12345'),
             'admin' => 1
         ]);
     }
